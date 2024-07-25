ac181574f111f0790dde6912724ec4468ae5f522
Robotico7883 <115508143+Robotico7883@users.noreply.github.com>
2024-07-25 17:46:08 -0500

    Add files via upload

---
+++ screens/Details.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet, Button, Alert } from "react-native";
+import { Card, Icon } from "react-native-elements";
+
+
+export default class DetailsScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      details: {},
+      imagePath: "",
+      url: ``
+    };
+  }
+
+  componentDidMount() {
+      // Llama a la función getDetails aquí para que los datos sean recuperados tan pronto como se despliegue la pantalla
+  }
+  getDetails = () => {
+      // Escribe el código para recuperar los datos de planetas específicos desde la API
+  };
+  /* Esta función determinará el estado de imagePath dependiendo de planetType*/
+  setDetails = (planetDetails) => {
+    const planetType = planetDetails.planet_type;
+    let imagePath = "";
+    switch (planetType) {
+      case "Gas Giant":
+        imagePath = require("../assets/Gas_Giant.png");
+        break;
+      case "Terrestrial":
+        imagePath = require("../assets/Terrestrial.png");
+        break;
+      case "Super Earth":
+        imagePath = require("../assets/Super_Earth.png");
+        break;
+      case "Neptune Like":
+        imagePath = require("../assets/Neptune-like.png");
+        break;
+      default:
+        imagePath = require("../assets/Gas_Giant.png");
+    }
+
+  this.setState({
+    details: planetDetails,
+    imagePath: imagePath,
+  });
+};
+  render() {
+    return
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  cardItem: {
+    marginBottom: 10
+  }
+});
